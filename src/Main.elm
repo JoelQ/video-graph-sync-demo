199212e09d98c6265a424a4326bf08b63e76e9a0
Joël Quenneville <joelq@thoughtbot.com>
2017-04-11 23:06:17 -0400

    Import EDA stream json

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,10 @@
+module Main exposing (..)
+
 import Html exposing (..)
+import Http
+import Json.Decode as JD exposing (Decoder)
+
+
 --import Html.Attributes exposing (..)
 --import Html.Events exposing (onClick)
 
@@ -17,18 +23,44 @@ main =
 -- MODEL
 
 
+type alias Point =
+    { x : Int, y : Int }
+
+
+type alias Stream =
+    { name : String
+    , points : List Point
+    }
+
+
 type alias Model =
-    {}
+    { position : Int, streams : List Stream }
 
 
 initialModel : Model
 initialModel =
-    {}
+    { position = 0, streams = [] }
+
+
+pointDecoder : Decoder Point
+pointDecoder =
+    JD.map2 Point (JD.field "x" JD.int) (JD.field "y" JD.int)
+
+
+streamDecoder : String -> Decoder Stream
+streamDecoder name =
+    JD.map2 Stream (JD.succeed name) (JD.list pointDecoder)
+
+
+fetchEda : Cmd Msg
+fetchEda =
+    Http.get "/eda.json" (streamDecoder "Eda")
+        |> Http.send ReceivedEda
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( initialModel, Cmd.none )
+    ( initialModel, fetchEda )
 
 
 
@@ -37,6 +69,7 @@ init =
 
 type Msg
     = NoOp
+    | ReceivedEda (Result Http.Error Stream)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -44,6 +77,12 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
+
+        ReceivedEda (Err message) ->
+            ( model, Cmd.none )
+
+        ReceivedEda (Ok stream) ->
+            ( { model | streams = stream :: model.streams }, Cmd.none )
 
 
 
@@ -61,7 +100,12 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text "Hello, world!"
-        , text (toString model)
+    main_ []
+        [ h1 [] [ text "Video Graph Sync Demo" ]
+        , section [] (List.map viewStream model.streams)
         ]
+
+
+viewStream : Stream -> Html a
+viewStream stream =
+    h2 [] [ text stream.name ]
